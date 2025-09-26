1dcc880afe342928405353ccfe9323c59266707d
Juliana Nunes <josecarloslmanunes30@gmail.com>
2025-09-26 20:41:36 -0300

    Atividade conlcuída

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿﻿string nome, sobrenome;
+Console.Write("Digite seu nome..........");
+nome = Console.ReadLine()!;
+Console.Write("Digite seu sobrenome.....");
+sobrenome = Console.ReadLine()!;
+Console.WriteLine($"Nome Completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+
